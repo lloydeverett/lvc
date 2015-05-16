8ca1596f8d2611c2321c384844ebf4843e267a39
Lloyd Everett <lloydeverett@gmail.com>
2015-05-16 23:18:31 +0200

    Initial Commit

---
+++ lvc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lvc
+//
+//  Created by Lloyd Everett on 2015/05/16.
+//  Copyright (c) 2015 Lloyd Everett. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
